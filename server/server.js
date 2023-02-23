b785cfa7cfdaa12ef2abb594a5b98db0bbae2f2b
Kenny Hung (he) <khung@instructors.2u.com>
2023-02-23 20:58:31 +0000

    BE working mongoose

--- server/server.js
+++ server/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { ApolloServer } = require('@apollo/server');
 const path = require('path');
+require('dotenv').config()
+
+const Profile = require('./models/Profile'); // TODO: Move this to the schemas
 //const { authMiddleware } = require('./utils/auth');
 
+const typeDefs = null
+const resolvers = null
 //const { typeDefs, resolvers } = require('./schemas');
-//const db = require('./config/connection');
+const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -32,21 +37,17 @@ app.get('/bum', (req, res) => {
   res.json('Hello world')
 })
 
-/*
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
-  await server.start();
-  server.applyMiddleware({ app });
+  // await server.start();
+  // server.applyMiddleware({ app });
   db.once('open', () => {
-  */
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       //console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
     })
-    /*
   })
-  };
-  
+};
+
 // Call the async function to start the server
-  startApolloServer(typeDefs, resolvers);
-*/
+startApolloServer(typeDefs, resolvers);
